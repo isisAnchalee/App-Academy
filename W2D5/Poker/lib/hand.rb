a6cacaeaa0a2c4b1e706afd8dc0f5f19687ef95b
‘isis_anchalee; GIT_AUTHOR_EMAIL=isiswenger@gmail.com <isiswenger@gmail.com>
2014-10-17 16:49:38 -0700

    Hand Winning Logic =^.^=

--- W2D5/Poker/lib/hand.rb
+++ W2D5/Poker/lib/hand.rb
@@ -5,7 +5,6 @@ class Hand
   attr_reader :hand_strength, :high_card
   
   HAND_RANKING =[
-
     :one_pair?, 
     :two_pair?, 
     :three_of_a_kind?,
@@ -27,12 +26,16 @@ class Hand
     @cards = cards
     @hand_strength = nil
     @high_card = 0
- 
   end
   
   def beats?(other_hand)    
-    @hand_strength > other_hand.hand_strength
-    
+    if @hand_strength > other_hand.hand_strength
+      return true
+    elsif @hand_strength == other_hand.hand_strength
+      return Card::POKER_VALUE[@high_card] > Card::POKER_VALUE[other_hand.high_card]
+    else
+      return false
+    end
   end
   
   def calculate_hand
@@ -53,14 +56,21 @@ class Hand
   
   def straight?
     (0...4).each do |idx|
-      return false if sort_hand[idx + 1].poker_value - sort_hand[idx].poker_value != 1
+      if sort_hand[idx + 1].poker_value - sort_hand[idx].poker_value != 1
+        @high_card = sort_hand.last
+        return true
+      end
     end
     
-    true
+    false
   end
   
   def flush?
-    @cards.all? { |card| card.suit == cards.first.suit }
+    if @cards.all? { |card| card.suit == cards.first.suit }
+      @high_card = sort_hand.last
+      return true
+    end
+    false
   end
   
   def royal_flush?
@@ -72,7 +82,13 @@ class Hand
     @cards.each do |card|
       card_counts[card.value] += 1
     end
-    card_counts.values.any? { |val| val == 4 }
+    card_counts.each_pair do |key, val|
+      if val == 4
+        @high_card = key
+        return true
+      end
+    end
+    false
   end
   
   def three_of_a_kind?
@@ -80,20 +96,35 @@ class Hand
     @cards.each do |card|
       card_counts[card.value] += 1
     end
-    card_counts.values.any? { |val| val == 3 }
+    card_counts.each_pair do |key, val|
+      if val == 3
+        @high_card = key
+        return true
+      end
+    end
+    false
   end
   
   def one_pair?
     card_counts = Hash.new(0)
+    repeats = 0
     
     @cards.each do |card|
       card_counts[card.value] += 1
     end
-    card_counts.values.one? { |val| val == 2 }
+    
+    card_counts.each_pair do |key, val|
+      if val == 2
+        repeats += 1
+        @high_card = key
+        return true if repeats == 1
+      end
+    end
+    false
   end
   
   def two_pair?
-    pairs = 0
+    pair_count, pairs = 0, []
     card_counts = Hash.new(0)
     
     @cards.each do |card|
@@ -101,14 +132,22 @@ class Hand
     end
     
     card_counts.each_pair do |k, v|
-      pairs += 1 if v == 2
+      pair_count += 1 if v == 2
+      pairs << k
     end
     
-    pairs == 2
+    if pair_count == 2
+      pairs.each do |pair|
+        pair = Card::POKER_VALUE[pair]
+      end
+      @high_card = pairs.sort.last
+      return true
+    end
+    false
   end
   
   def full_house?
-    three_of_a_kind? && one_pair?
+     one_pair? && three_of_a_kind?
   end
   
   def high_card
